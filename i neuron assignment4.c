047120d12f5b4fbf1e64ed54491fddcf328604e6
Shiva1231234 <109754082+Shiva1231234@users.noreply.github.com>
2022-08-15 06:42:34 +0530

    Add files via upload

---
+++ i neuron assignment4.c
@@ -0,0 +1,159 @@
+Assignment =4
+Question =1
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 5; ++i) {
+    printf(" my sirg\n");
+  }
+  return 0;
+}
+*\
+
+
+Question =2
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf(" %d\n",i);
+  }
+  return 0;
+}
+*\
+*\
+Question =3
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = n; i >= 1; --i) {
+    printf(" %d\n",i);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =4
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf(" %d\n",2*i-1);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =5
+/*
+/*
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 10; i >= 1; --i) {
+    printf(" %d\n",2*i-1);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =6
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf(" %d\n",2*i);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =7
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 10; i>= 1; --i) {
+    printf(" %d\n",2*i);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =8
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i<= 10; ++i) {
+    printf(" %d\n",i*i);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =9
+/*
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i<= 10; ++i) {
+    printf(" %d\n",i*i*i);
+  }
+  return 0;
+}
+*\
+*\
+
+Question =10
+/*
+/*
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i<= 10; ++i) {
+    printf(" %d\n",5*i);
+  }
+  return 0;
+}
+*\
+*\
